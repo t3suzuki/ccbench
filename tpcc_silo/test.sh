2a3ac6be507ceb606a31a8a641e9430b23fa92f6
Tomoya Suzuki <t3suzuki@ucsd.edu>
2022-12-27 23:42:31 +0000

    update test script for PILO tests.

--- tpcc_silo/test.sh
+++ tpcc_silo/test.sh
@@ -3,15 +3,16 @@
 nth_list="1 8"
 ntrial=3
 trus_list="1 4 16 64"
-ncoro_list="1 4 16 64 256"
-opts="-perc_payment 0"
+ncoro_list="1 2 4 8 16 32 64 128 256 512"
+#opts="-perc_payment 0  -extime 10"
+opts="-extime 5"
 
 for nth in $nth_list
 do
     for itrial in `seq $ntrial`
     do
 	echo "mode=original nth=$nth itrial=$itrial"
-	taskset -c 0-9 ./silo_original -thread_num $nth -extime 10 $opts
+	taskset -c 0-9 ./silo_original -thread_num $nth $opts
     done
 done
 
@@ -24,7 +25,9 @@ do
 	    for itrial in `seq $ntrial`
 	    do
 		echo "mode=corobase+new_pref trus=$trus nth=$nth ncoro=$ncoro itrial=$itrial"
-		taskset -c 0-9 ./corobase_${trus}us_${ncoro}coro -thread_num $nth -extime 10 $opts
+		taskset -c 0-9 ./corobase_${trus}us_${ncoro}coro -thread_num $nth $opts
+		echo "mode=pilo trus=$trus nth=$nth ncoro=$ncoro itrial=$itrial"
+		taskset -c 0-9 ./pilo_${trus}us_${ncoro}coro -thread_num $nth $opts
 	    done
 	done
     done
